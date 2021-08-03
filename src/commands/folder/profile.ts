6a80a8ac28fd8ba4879e2e32f7b84d30f56580c3
Todd <teese.todd@gmail.com>
2021-08-03 16:57:43 +0100

    positive tests with file creation/cleanup

--- src/commands/folder/profile.ts
+++ src/commands/folder/profile.ts
@@ -31,23 +31,24 @@ export default class Profile extends SfdxCommand {
     public async run(): Promise<AnyJson> {
         const source: string = this.flags.source;
         this.target = this.flags.target;
-        if(!await this.hasAccess(source)) {
+        if (!await this.hasAccess(source)) {
             throw new SfdxError(messages.getMessage('errorSource'));
         }
 
         this.ux.startSpinner('Creating Profiles');
-        this.getProfiles(source);
+        this.getProfiles(source).catch(error => {
+            throw new SfdxError('Issue in Creating Profiles');
+        });
         this.ux.stopSpinner('Done');
         this.ux.log('Created Profiles in Folder ' + this.target);
 
-
-        return {};
+        return true;
     }
 
-    private async hasAccess(path: string) : Promise<boolean>  {
+    private async hasAccess(path: string): Promise<boolean>  {
         let canAccess: boolean = true;
         try {
-            fs.accessSync(path, (fs.constants.F_OK | fs.constants.W_OK))
+            fs.accessSync(path, (fs.constants.F_OK | fs.constants.W_OK));
         } catch (error) {
             canAccess = false;
         }
@@ -55,11 +56,13 @@ export default class Profile extends SfdxCommand {
     }
 
     private async getProfiles(path) {
-        if(this.hasAccess(path)) {
-            let profileDirectories = fs.readdirSync(path);
-            for(let i = 0 ; i < profileDirectories.length; i++) {
-                const directory = profileDirectories[i];
-                this.getProfileFiles(path+'\\'+directory, directory);
+        if (this.hasAccess(path)) {
+            const profileDirectories = fs.readdirSync(path);
+            for (const directory of profileDirectories) {
+                this.getProfileFiles( path + '\\' + directory, directory).catch(error => {
+                    console.error(error);
+                    throw new SfdxError('Error in Profile Generation');
+                });
             }
         }
     }
@@ -72,34 +75,33 @@ export default class Profile extends SfdxCommand {
             canAccess = false;
         }
 
-        if(fs.statSync(path).isFile()) {
+        if (fs.statSync(path).isFile()) {
             return;
         }
-    
-        if(canAccess && !fs.statSync(path).isFile()) {
+        if (canAccess && !fs.statSync(path).isFile()) {
             let profileFiles;
 
             profileFiles = fs.readdirSync(path);
 
-            let profileValue = {};
-            for(let i = 0; i < profileFiles.length; i++) {
-                const file = profileFiles[i];
+            const profileValue = {};
+            for (const file of profileFiles) {
                 const fileType = file.substring(file.lastIndexOf('.'));
                 const attribute = file.substring(0, file.lastIndexOf('.'));
-                if(fileType == '.csv') {
-                    const csvData = await getCsvValues(path+'\\'+file);
-                    let attributeArray = csvToArray(csvData);
+                if (fileType === '.csv') {
+                    const csvData = await getCsvValues(path + '\\' + file);
+                    const attributeArray = csvToArray(csvData);
                     profileValue[attribute] = attributeArray;
                 }
-                if(fileType == '.json') {
-                    const jsonData = await getJsonValues(path+'\\'+file);
-                    if(jsonData) {
+                if (fileType === '.json') {
+                    const jsonData = await getJsonValues(path + '\\' + file);
+                    if (jsonData) {
                         applyJsonValues(jsonData, profileValue);
                     }
-                } 
+                }
             }
-            convertProfileToXml(profileName, profileValue, this.target);
+            convertProfileToXml(profileName, profileValue, this.target).catch(error => {
+                throw new SfdxError('Issue in Profile Generation');
+            }) ;
         }
-    
     }
 }
--- test/commands/folder/profile.test.ts
+++ test/commands/folder/profile.test.ts
@@ -1,3 +1,51 @@
-// import { expect, test } from '@salesforce/command/lib/test';
+import { test } from '@salesforce/command/lib/test'; //expect
+import fs = require('fs');
 
-describe('folder:profile', () => {});
+const objectPermissions = 'object,enabled\ntestObject,true';
+const otherAttributes = '{"custom":true}';
+
+const testPath = '.\\TestProfiles';
+const testPathAdmin = '.\\TestProfiles\\Admin';
+const testPathAdminObjectPermissions = '.\\TestProfiles\\Admin\\objectPermissions.csv';
+const testPathAdminOtherAttributes = '.\\TestProfiles\\Admin\\otherAttributes.json';
+
+const testOutputPath = '.\\TestProfilesOutput';
+const testOutputAdminPath = '.\\TestProfilesOutput\\Admin.profile-meta.xml';
+
+describe('Hooks', () => {
+    before(async () => {
+        await fs.mkdir(testPath, async err => {
+            await fs.mkdir(testPathAdmin, async err => {
+                await fs.writeFile(testPathAdminObjectPermissions, objectPermissions, err => {})
+                await fs.writeFile(testPathAdminOtherAttributes, otherAttributes, err => {})
+            })
+        })
+    });
+
+    after(async () => {
+
+        await fs.unlink(testPathAdminObjectPermissions, async (err) => {
+            await fs.unlink(testPathAdminOtherAttributes, async err => {
+                await fs.rmdir(testPathAdmin, async err => {
+                    await fs.rmdir(testPath, async err => {
+                        await fs.unlink(testOutputAdminPath, async err => {
+                            await fs.rmdir(testOutputPath, async err => {})
+                        })
+                    })
+                })
+            })
+        })
+
+
+
+
+
+    });
+
+    describe('folder:profile', () => {
+        test
+        .command(['folder:profile', '-s', testPath, '-t', testOutputPath])
+        .it('runs folder:profile -s ./TestProfiles -t ./TestProfilesOutput', () => {})
+    });
+})
+
